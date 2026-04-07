4b1c2ba5d817614400c6aff362177f6806b9ed14
anandhit27 <anandrajanand573@gmail.com>
2026-04-07 19:18:08 +0530

    Delete Basic-ChatBot-anand raj directory

--- Basic-ChatBot-anand raj/src/agents/chat_agent/states/chat_agent_state.py
+++
@@ -1,9 +0,0 @@
-from typing_extensions import TypedDict, Annotated
-from langchain.messages import AnyMessage
-import operator
-
-class ChatAgentState(TypedDict):
-    """
-    This is a class
-    """
-    messages  : Annotated[list[AnyMessage], operator.add]
